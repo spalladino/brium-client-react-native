c69dfab9a1c2a824477f2947e91b0d4683e23438
Santiago Palladino <spalladino@gmail.com>
2016-04-07 14:47:59 -0300

    Render current activity

--- index.android.js
+++ index.android.js
@@ -20,35 +20,81 @@ import {
 require("./array");
 
 const ENTRIES_URL = `https://brium.me/api/entries.json?worker_id=${BRIUM_WORKER_ID}&since=2016-04-01&access_token=${BRIUM_TOKEN}`;
+const MESSAGE_URL = `https://brium.me/api/messages?access_token=${BRIUM_TOKEN}`;
 
-const styles = StyleSheet.create({
+let styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    padding: 10
   },
-  keyword: {
-    flex: 1,
-    alignItems: 'center'
+  currentActivity: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  currentActivityKeyword: {
+    fontSize: 24
   }
 });
+
+styles.keyword = StyleSheet.create({
+  item: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 6
+  },
+  name: {
+  },
+  list: {
+    flex: 1
+  }
+})
 
 class BriumClient extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loaded: false
+      loaded: false,
+      currentActivity: { loaded: false }
     };
   }
 
   componentDidMount() {
-    this.fetchEntries();
+    this.loadKeywords();
+    this.loadCurrentActivity();
   }
 
-  fetchEntries() {
-    fetch(ENTRIES_URL)
-      .then((response) => response.json())
+  postMessage(msg) {
+    return fetch(MESSAGE_URL, {method: 'POST', body: msg})
+      .then((response) => response.text());
+  }
+
+  loadCurrentActivity() {
+    this.fetchCurrentActivity()
+      .then((activity) => this.setState({
+        currentActivity: {
+          loaded: true,
+          keyword: activity ? activity.keyword : null,
+          timeAgo: activity ? activity.timeAgo : null
+        }
+      }))
+      .done();
+  }
+
+  fetchCurrentActivity() {
+    const currentActivityRegex = /You said you were working on (.+?) ((?:about |less than )?\d+ (?:minutes?|hours?) ago)/;
+    return this.postMessage('?')
+      .then((response) => {
+        let match = response.match(currentActivityRegex);
+        return match ? { keyword: match[1], timeAgo: match[2] } : null;
+      });
+  }
+
+  loadKeywords() {
+    this.fetchEntries()
       .then((responseData) => {
         this.setState({
           keywords: responseData.map((e) => e.record).reverse().unique().slice(0,5),
@@ -58,12 +104,18 @@ class BriumClient extends Component {
       .done();
   }
 
+  fetchEntries() {
+    return fetch(ENTRIES_URL)
+      .then((response) => response.json());
+  }
+
   render() {
     if (!this.state.loaded) {
       return this.renderLoadingView();
     }
     return (
       <View style={styles.container}>
+        <CurrentActivity activity={this.state.currentActivity} />
         <KeywordsList keywords={this.state.keywords} />
       </View>
     );
@@ -76,6 +128,24 @@ class BriumClient extends Component {
       </View>
     );
   }
+}
+
+class CurrentActivity extends Component {
+
+  render() {
+    if (!this.props.activity.loaded) {
+      return <View></View>;
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.currentActivity}>Working on</Text>
+        <Text style={styles.currentActivity, styles.currentActivityKeyword}>{this.props.activity.keyword}</Text>
+        <Text style={styles.currentActivity}>since {this.props.activity.timeAgo}</Text>
+      </View>
+    );
+  }
+
 }
 
 class KeywordsList extends Component {
@@ -94,15 +164,15 @@ class KeywordsList extends Component {
       <ListView
         dataSource={this.state.dataSource}
         renderRow={this.renderKeyword}
-        style={styles.listView}
+        style={styles.keyword.list}
       />
     );
   }
 
   renderKeyword(keyword) {
     return (
-      <View style={styles.keyword}>
-        <Text>{keyword}</Text>
+      <View style={styles.keyword.item}>
+        <Text style={styles.keyword.name}>{keyword}</Text>
       </View>
     );
   }
